865f9d617c255ffa9507dcffb8841f9907530886
FRC6302 <frc6302@gmail.com>
2022-01-21 18:14:46 -0500

    cleaning

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -21,10 +21,6 @@ public class Shooter extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  //hi
-  //asdf
-
-  //new feature 3
 
   public void shootWithInitialBallVelocity(double paraV, double perpV, double hoodAngle, double offsetAngle, double distance) {
     //offsetAngle = 10 * perpV;
